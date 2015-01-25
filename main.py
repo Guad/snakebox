39f40554a4fc30870112825994c72b41f36c2aa8
Felipe Perez <felipe@chichasov.es>
2015-01-25 01:31:29 +0100

    Fixed typo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import transmissionrpc
-import Flask
+import flask
 from werkzeug import secure_filename
 
 
@@ -9,7 +9,7 @@ with open('config.ini', 'r') as file:
         line = line.split('==')
         config[line[0]] = line[1]
 
-app = Flask.Flask(__name__)  # Initialize our application
+app = flask.Flask(__name__)  # Initialize our application
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024  # Set the upload limit to 1MiB
 app.secret_key = config['SECRET_KEY']
 
@@ -22,20 +22,20 @@ def processNewTorrent(torrentfile):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if Flask.request.method == 'POST':
-        uploaded = Flask.request.files.getlist("file[]")
+    if flask.request.method == 'POST':
+        uploaded = flask.request.files.getlist("file[]")
         for f in uploaded:
             if secure_filename(f.filename):
                 f.save('static/torrents/%s' % secure_filename(f.filename))
                 print 'Uploaded file "%s"' % secure_filename(f.filename)
-                Flask.flash(Flask.Markup('Successfully uploaded torrent %s. Download starting...') % secure_filename(
+                flask.flash(flask.Markup('Successfully uploaded torrent %s. Download starting...') % secure_filename(
                     f.filename))
             else:
-                Flask.flash('Invalid filename.', Flask.url_for(index))
+                flask.flash('Invalid filename.', flask.url_for(index))
 
-        return Flask.redirect(Flask.url_for('index'))  # Upload done, refresh.
+        return flask.redirect(flask.url_for('index'))  # Upload done, refresh.
     else:
-        return Flask.render_template('index.html', torrents=tc.get_torrents())
+        return flask.render_template('index.html', torrents=tc.get_torrents())
 
 
 if __name__ == '__main__':
